504c01c39f39444128f483e3aeadb5b2c3de14a6
Aurélien <age@bepark.be>
2019-10-01 22:19:17 +0200

    first repo

---
+++ app/Repository/AddressRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository\Parking;
+
+use App\Models\Database\Client;
+use App\Repository\AbstractRepository;
+use Illuminate\Support\Collection;
+
+class AddressRepository extends AbstractRepository
+{
+    /**
+     * ClientRepository constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
