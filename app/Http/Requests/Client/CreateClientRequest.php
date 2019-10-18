6f22d762ec5c3686ca439e899d18d4b8320a86ba
Aurélien <age@bepark.be>
2019-10-18 19:50:06 +0200

    request update

--- app/Http/Requests/Client/CreateClientRequest.php
+++ app/Http/Requests/Client/CreateClientRequest.php
@@ -12,13 +12,17 @@ class CreateClientRequest extends FormRequest
 	{
         return [
             'firstname' => 'required|string|max:50',
-            'name' => 'required|string|max:50',
+            'lastname' => 'required|string|max:50',
             'email' => 'required|string|max:75',
             'language' => 'required|string|max:5',
             'phone_prefix' => 'required|string|max:5',
             'mobile_prefix' => 'required|string|max:5',
             'phone_number' => 'required|numeric',
             'mobile_number' => 'required|numeric',
+            'address.street' => 'required|string|max:150',
+            'address.number' => 'required|numeric',
+            'address.city' => 'required|string|max:50',
+            'address.country' => 'required|string|max:50',
         ];
     }
 
