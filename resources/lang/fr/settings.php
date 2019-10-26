10f17eb81aed83c3b59bdfaf1f43e9bf5b86f23c
Aurélien <age@bepark.be>
2019-10-27 01:59:42 +0200

    trad settings

---
+++ resources/lang/fr/settings.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    'show' => [
+        'title' => 'Paramètres de l\'application',
+        'back' => 'Retour',
+        'edit' => 'Editer les paramètres',
+        'information' => [
+            'title' => 'Affichage des paramètres',
+            'subtitle' => 'Frais fixe et variables des vendeurs',
+            'fixed_fee' => 'Frais fixes',
+            'variable_fee' => 'Frais variables',
+        ],
+    ],
+    'edit' => [
+        'title' => 'Edition des Paramètres de l\'application',
+        'subtitle' => 'Modification des frais pour le vendeur',
+        'help' => 'Renseigner les frais pour les vendeurs',
+        'fixed_fee_label' => 'Frais fixes',
+        'variable_fee_label' => 'Frais variables',
+        'fixed_fee' => '1',
+        'variable_fee' => '0.1 = 10%',
+    ],
+];
---
+++ resources/lang/fr/statistics.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+
+    'show' => [
+        'title' => 'Description d\'un Ticket',
+        'back' => 'Retour',
+        'information' => [
+            'title' => 'Information du Ticket',
+            'subtitle' => 'Toutes les informations du ticket',
+            'client_firstname' => 'Prénom du client',
+            'client_lastname' => 'Nom du client',
+            'total_price' => 'Prix total',
+            'number_product' => 'Nombre de produit(s) dans le ticket',
+        ],
+    ],
+];
