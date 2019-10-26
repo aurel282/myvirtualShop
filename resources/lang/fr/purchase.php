7f2377a5f9b33468be94cc4f005ed923d138c2ba
Aurélien <age@bepark.be>
2019-10-26 23:36:09 +0200

    achat trad

--- resources/lang/fr/purchase.php
+++ resources/lang/fr/purchase.php
@@ -13,6 +13,16 @@ return [
     |
     */
 
+    'create' => [
+        'title' => 'Ajout d\'un achat',
+        'subtitle' => 'Ajouter un achat dans un ticket',
+        'help' => 'Veuillez renseigner le code du jouet ( Attention à être en majuscule pour le scan! )',
+        'code_label' => 'Code du jouet',
+        'code' => '001025000085',
+        'quantity_label' => 'Quantité',
+        'quantity' => '1',
+    ],
+
     'list' => [
 
     ],
