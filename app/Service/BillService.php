3728ce9a5c9de5abc185db3f3ca7ad14d9a6569d
Aurélien <age@bepark.be>
2019-10-27 11:20:43 +0100

    start export bill

--- app/Service/BillService.php
+++ app/Service/BillService.php
@@ -32,4 +32,49 @@ class BillService extends AbstractService
     {
         return $this->_billRepository->getList();
     }
+
+    public function exportBill(Bill $bill)
+    {
+
+        $purchases = $bill->purchases()->get();
+        $client = $bill->client();
+
+        //$total_sold = $this->getTotalSold($provider);
+
+        $f = fopen('php://memory', 'w');
+        // loop over the input array
+        $data =  [
+            $client->id,
+            utf8_encode($client->name),
+            utf8_encode($client->firstname),
+        ];
+        // generate csv lines from the inner arrays
+        fputcsv($f, $data, ',');
+        fputcsv($f, [], ',');
+
+        fputcsv($f, ['Total Titcket'], ',');
+        fputcsv($f, [$total_sold], ',');
+
+
+        fputcsv($f, ['Articles: '. count($products_sold)], ',');
+
+        foreach ($products_sold as $product_sold) {
+            $data =  [
+                $product_sold->price_per_unity,
+                utf8_encode($product_sold->name),
+                utf8_encode($product_sold->code),
+            ];
+            // generate csv lines from the inner arrays
+            fputcsv($f, $data, ',');
+        }
+
+        // reset the file pointer to the start of the file
+        fseek($f, 0);
+        // tell the browser it's going to be a csv file
+        header('Content-Type: application/csv');
+        // tell the browser we want to save it instead of displaying it
+        header('Content-Disposition: attachment; filename=providers.csv;');
+        // make php send the generated csv lines to the browser
+        fpassthru($f);
+    }
 }
